1ba08c878a5098c6bbbc91c734eefc2bf2a90e49
Michaelflip <michaelflip@gmail.com>
2016-07-03 09:12:27 -0700

    slight change to admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import Post
 
 class PostModelAdmin(admin.ModelAdmin):
-	list_display = ["__unicode__", "updated", "timestamp"]
+	list_display = ["title", "updated", "timestamp"]
 	list_display_links = ["updated"]
 	list_editable = ["title"]
 	list_filter = ["updated", "timestamp"]
