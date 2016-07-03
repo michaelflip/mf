db269c3ddae947d8b8dccc2ca61dd463119a3eb9
Michaelflip <michaelflip@gmail.com>
2016-07-03 10:22:06 -0700

    test 3

--- posts/urls.py
+++ posts/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import(
+from .views import (
 	post_list,
 	post_create,
 	post_detail,
