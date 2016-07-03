670d2cf64c44b637e5a5063fb7f508335e889417
Michaelflip <michaelflip@gmail.com>
2016-07-03 10:23:41 -0700

    urls.py change

--- posts/urls.py
+++ posts/urls.py
@@ -26,10 +26,10 @@ from .views import (
 	)
 
 urlpatterns = [
-	url(r'^$', "post_list"),
-    url(r'^create/$', "post_create"),
-    url(r'^detail/$', "post_detail"),
-    url(r'^update/$', "post_update"),
-    url(r'^delete/$', "post_delete"),
+	url(r'^$', post_list),
+    url(r'^create/$', post_create),
+    url(r'^detail/$', post_detail),
+    url(r'^update/$', post_update),
+    url(r'^delete/$', post_delete),
 
 ]
