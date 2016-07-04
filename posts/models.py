6c8e075951a0b785d8382e380f81a38a69af4cea
Michaelflip <michaelflip@gmail.com>
2016-07-03 18:19:27 -0700

    post detail changes 2

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.core.urlresolvers import reverse
+
 # Create your models here.
 
 
@@ -16,3 +18,7 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse("posts:detail", kwargs={"id": self.id})
+		#return "/posts/%s/" %(self.id)
