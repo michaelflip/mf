b0753ac659b443936c0d5bab6057172ef4dec1ff
Michaelflip <michaelflip@gmail.com>
2016-07-03 18:02:42 -0700

    post detail changes

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -8,12 +8,15 @@ from .models import Post
 def post_create(request):
 	return HttpResponse("<h1>Create</h1>")
 
-def post_detail(request):
+def post_detail(request, id=None):
+	#instance = Post.objects.get(id=3)
+	instance = get_object_or_404(Post, id=id)
 	context = {
-		"title": "Detail"
+		"title": instance.title,
+		"instance": instance,
 	}
 
-	return render(request, "index.html", context)
+	return render(request, "post_detail.html", context)
 
 def post_list(request):
 	queryset = Post.objects.all()
