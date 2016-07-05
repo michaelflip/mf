1c2d21926c790b6215c8569fc4b3677b95327f6a
michaelflip <michaeladamlewis@gmail.com>
2016-07-05 09:34:26 -0700

    static 2

--- mfdotcom/settings.py
+++ mfdotcom/settings.py
@@ -126,4 +126,4 @@ STATICFILES_DIRS = [
     # '/var/www/static/',
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR, "static_cdn")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
