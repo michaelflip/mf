ea188dbe5c89d5c00e5ea083370a2849e5cc1376
michaelflip <michaeladamlewis@gmail.com>
2016-07-06 07:17:30 -0700

    added filefield

--- posts/models.py
+++ posts/models.py
@@ -9,6 +9,7 @@ from django.core.urlresolvers import reverse
 
 class Post(models.Model):
 	title = models.CharField(max_length=200)
+	image = models.FileField(null=True, blank=True)
 	content = models.TextField()
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
