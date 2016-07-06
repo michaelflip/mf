ca4d18533b364dd2247a06e9733cc7d14896a9d3
michaelflip <michaeladamlewis@gmail.com>
2016-07-06 09:36:15 -0700

    couldnt figure it out

--- posts/models.py
+++ posts/models.py
@@ -1,47 +1,47 @@
 from __future__ import unicode_literals
 
 
-
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
 
 from django.utils.text import slugify
-
-
 # Create your models here.
+# MVC MODEL VIEW CONTROLLER
+
 
 def upload_location(instance, filename):
-	# filebase, extension = filename.split(".")
-	# return "%s/%s.%s" %(instance.id, instance.id, filename)
-	return "%s/%s" %(instance.id, filename)
-
+    #filebase, extension = filename.split(".")
+    #return "%s/%s.%s" %(instance.id, instance.id, extension)
+    return "%s/%s" %(instance.id, filename)
 
 class Post(models.Model):
-	title = models.CharField(max_length=120)
-	slug = models.SlugField(unique=True)
-	image = models.ImageField(upload_to=upload_location,
-		null=True, 
-		blank=True, 
-		width_field="width_field", 
-		height_field="height_field")
-	height_field = models.IntegerField(default=0)
-	width_field = models.IntegerField(default=0)
-	content = models.TextField()
-	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
-	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to=upload_location, 
+            null=True, 
+            blank=True, 
+            width_field="width_field", 
+            height_field="height_field")
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
-	def __unicode__(self):
-		return self.title 
+    def __unicode__(self):
+        return self.title
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
-	def get_absolute_url(self):
-		return reverse("posts:detail", kwargs={"slug": self.slug})
-		#return "/posts/%s/" %(self.id)
-	class Meta:
-		ordering = ["-timestamp", "-updated"]
+    def get_absolute_url(self):
+        return reverse("posts:detail", kwargs={"slug": self.slug})
+
+    class Meta:
+        ordering = ["-timestamp", "-updated"]
+
+
 
 def create_slug(instance, new_slug=None):
     slug = slugify(instance.title)
@@ -55,14 +55,9 @@ def create_slug(instance, new_slug=None):
     return slug
 
 
-
-
 def pre_save_post_receiver(sender, instance, *args, **kwargs):
-	if not instance.slug:
-		instance.slug = create_slug(instance)
-
-
-
+    if not instance.slug:
+        instance.slug = create_slug(instance)
 
 
 
