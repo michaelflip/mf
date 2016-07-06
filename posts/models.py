613bc0975ffd04c7cafea9622a75fe62508884ed
michaelflip <michaeladamlewis@gmail.com>
2016-07-06 08:57:48 -0700

    added slugs

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,13 @@
 from __future__ import unicode_literals
 
-from django.db import models
+
 
 from django.core.urlresolvers import reverse
+from django.db import models
+from django.db.models.signals import pre_save
+
+from django.utils.text import slugify
+
 
 # Create your models here.
 
@@ -30,7 +35,25 @@ class Post(models.Model):
 		return self.title
 
 	def get_absolute_url(self):
-		return reverse("posts:detail", kwargs={"id": self.id})
+		return reverse("posts:detail", kwargs={"slug": self.slug})
 		#return "/posts/%s/" %(self.id)
 	class Meta:
 		ordering = ["-timestamp", "-updated"]
+
+
+def create_slug(instance, new_slug=None):
+	slug = slugify(instance.title)
+	if new_slug is not None:
+		slug = new_slug
+	qs = Post.objects.filter(slug=slug).order_by("-id")
+	exists = qs.exists()
+	if exists:
+		new_slug = "%s-%s" %(slug, sq.first().id)
+		return create_slug(instance, new_slug=new_slug)
+	return slug
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+	if not instance.slug:
+		instance.slug = create_slug(instance)
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
