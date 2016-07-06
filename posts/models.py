5a66598440a6ea3477afcfb72ec3882d02e5116e
michaelflip <michaeladamlewis@gmail.com>
2016-07-06 07:31:30 -0700

    imagefield added migrate test

--- posts/models.py
+++ posts/models.py
@@ -9,7 +9,11 @@ from django.core.urlresolvers import reverse
 
 class Post(models.Model):
 	title = models.CharField(max_length=200)
-	image = models.FileField(null=True, blank=True)
+	image = models.ImageField(null=True, blank=True, 
+		width_field="width_field", 
+		height_field="height_field")
+	height_field = models.IntegerField(default=0)
+	width_field = models.IntegerField(default=0)
 	content = models.TextField()
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
